94b12e9976e9c19a03c50d606e26ef53f0d0fa5f
Oliv4945 <oliv4945@gmail.com>
2017-04-14 10:59:48 +0200

    Get username & enable at statup

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 # Config
 systemd_path="/etc/systemd/system/"
 service_filename="jarvis.service"
+username=`whoami` # Jarvis username variable is not set ?
 
 # Copy file and configure it with Jarvis data
 sudo cp $service_filename $systemd_path
@@ -11,4 +12,4 @@ sudo sed -i "s!PATH_TO_JARVIS!$jv_dir!" $systemd_path$service_filename
 sudo sed -i "s!USERNAME!$username!" $systemd_path$service_filename
 
 # Enable at startup
-#sudo systemctl enable jarvis
+sudo systemctl enable jarvis
