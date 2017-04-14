81175a035d150733e840a679b4dd10d953174db3
Oliv4945 <oliv4945@gmail.com>
2017-04-14 11:09:40 +0200

    Add uninstallation script

--- uninstall.sh
+++ uninstall.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 # Use only if you need to undo changes on the user system such as removing software
+
+# Config
+systemd_path="/etc/systemd/system/"
+service_filename="jarvis.service"
+
+# Remove statup link
+sudo systemctl disable jarvis
+# Stop Jarvis
+sudo systemctl stop jarvis
+# Remove file
+sudo rm $systemd_path$service_filename
