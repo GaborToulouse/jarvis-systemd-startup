b137b08fddbbab19923ee713fe552bd424c25ae2
Oliv4945 <oliv4945@gmail.com>
2017-04-14 10:41:08 +0200

    Installation script

--- install.sh
+++ install.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 # Use only if you need to perform changes on the user system such as installing software
+
+# Config
+systemd_path="/etc/systemd/system/"
+service_filename="jarvis.service"
+
+# Copy file and configure it with Jarvis data
+sudo cp $service_filename $systemd_path
+sudo sed -i "s!PATH_TO_JARVIS!$jv_dir!" $systemd_path$service_filename
+sudo sed -i "s!USERNAME!$username!" $systemd_path$service_filename
+
+# Enable at startup
+#sudo systemctl enable jarvis
